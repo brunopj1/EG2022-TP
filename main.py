96b3393388800aa515e834d766f1f81099d88f47
brunopj <brunopj1@hotmail.com>
2022-05-29 16:04:13 +0100

    ultimo exemplo

--- main.py
+++ main.py
@@ -6,75 +6,15 @@ from interpreter_sdg import InterpreterSDG
 from report_generator import generateReport
 
 codigo = """
-void fun_if() {
-    int a = 1;
-    int b = 2;
-    int c = a + b;
-    if (True) { }
-    else if (True) {
-        if (False) {
-            a++;
-        }
-        else {
-            b++;
-            a--;
-        }
-        b++;
-    }
-    else { }
-    a++;
-}
-
-void fun_while() {
-    int a = 0;
-    while (a < 100) {
-        a++;
-        if (a == 50) {
-            a += 10;
+void main() {
+    int i = 0;
+    int max = 100;
+    int res = 0.5;
+    while (i < max) {
+        if (i % 2) {
+            res += i;
         }
     }
-    a = 100;
-}
-
-void fun_do_while() {
-    int a = 0;
-    do {
-        a++;
-        if (a == 50) {
-            a += 10;
-        }
-    } while (a < 100);
-    a = 100;
-}
-
-void fun_for() {
-    int a = 1;
-    int b = 0;
-    for (int i = 0; i < 10; i++) {
-        a *= i;
-        if (a % 2 == 0) {
-            b++;
-        }
-        else {
-            b--;
-        }
-    }
-    a = 100;
-}
-
-void fun_foreach() {
-    int a = 1;
-    int b = 0;
-    foreach (int i in [1,2,3,4,5]) {
-        a *= i;
-        if (a % 2 == 0) {
-            b++;
-        }
-        else {
-            b--;
-        }
-    }
-    a = 100;
 }
 """
 
